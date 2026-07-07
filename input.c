76ba80b2cd2ab224fa9318991364b1c737ef3892
kshitija <kshitijagandge477@gmail.com>
2026-07-07 19:06:36 +0530

    add c input programme

---
+++ input.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+    char name[50],college[100];
+    int age;
+    printf("name=");
+    scanf("%49s",name);
+
+    printf("age =");
+    scanf("%d",&age);
+
+    printf("college =");
+    scanf("%99s",college);
+
+    return 0;
+}
