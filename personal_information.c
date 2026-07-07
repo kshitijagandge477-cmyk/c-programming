df71bf07a77a68416550b940bd0e24387b086a55
kshitija <kshitijagandge477@gmail.com>
2026-07-07 18:14:10 +0530

    Add personal information program in c

---
+++ personal_information.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main(){
+    printf("Name = Kshitija satish gandge\n");
+    printf("Age = 20\n");
+    printf("Collage = JNEC\n");
+    printf("Branch = computer science engineering\n ");
+    return 0;
+ }
