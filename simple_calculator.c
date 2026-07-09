0aa22bd6ae8bbe813caa89ec4bfb6349ea601f58
kshitija <kshitijagandge477@gmail.com>
2026-07-09 21:31:39 +0530

    simple calculator program

---
+++ simple_calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    printf("enter num 1 =");
+    scanf("%d",&num1);
+    printf("enter num2 = ");
+    scanf("%d",&num2);
+    printf("addition = %d\n",num1+num2);
+    printf("substraction = %d\n",num1-num2);
+    printf("multiplication = %d\n",num1*num2);
+    printf("division = %d\n",num1/num2);
+    printf("remainder = %d\n",num1%num2);
+    return 0;
+}
